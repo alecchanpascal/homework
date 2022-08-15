5fa748e0ead5c1fea189adf86ff2e2d537feb822
Alec Chan <alec.pascal@gmail.com>
2022-08-15 01:41:04 +0000

    got functionality up to win and lose page and started working on having new rounds

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
-let word = 0;
-const space = String.fromCharCode(20);
+let word = 0
+let guess = 0
+const space = String.fromCharCode(20)
 const words = [
     "stranger",
+    "nothing"
 ]
 
-$(document).ready(function() {
+$(document).ready(hangman = () => {
     $('img').hide()
     $('#0').show()
+    $('h1').remove()
+    $('button').removeClass('disabled')
+    $('button').removeClass('highlight')
     words[word] = words[word].toUpperCase()
+    let left = words[word].length
     for (let i = 0; i < words[word].length; i++){
-        $('#letters').append(`<h1 id="remove">${space}</h1>`)
+        $('#letters').append(`<h1>${space}</h1>`)
         $('#letters').append(`<h1>${space}</h1>`)
         $('#letters').append(`<h1 class="${i}">${space}</h1>`)
         $('#letters').append(`<h1 id="end">${space}</h1>`)
@@ -18,8 +24,41 @@ $(document).ready(function() {
         $(this).addClass('highlight')
         $(this).addClass('disabled')
         if (words[word].includes($(this).html())){
-            $(`h1.${(words[word].indexOf($(this).html()))}`).prev().remove()
-            $(`h1.${(words[word].indexOf($(this).html()))}`).html($(this).html())
+            for (let i = 0; i < words[word].length; i++){
+                if (words[word][i] === $(this).html()){
+                    $(`h1.${i}`).prev().remove()
+                    $(`h1.${i}`).html($(this).html())
+                    left--
+                }
+            }
+            if (left === 0){
+                word++
+                if (word >= words.length){
+                    alert("You Won!")
+                } else {
+                    if (confirm("You Win!\nWould You Like to Keep Playing?")){
+                        guess = 0
+                        hangman()
+                    } else {
+                        $('#buttons').hide()
+                    }
+                    // display a win page or something?
+                }
+            }
+        } else {
+            $(`#${guess}`).hide()
+            guess++
+            $(`#${guess}`).show()
+            if (guess === 6){
+                if (confirm("You Lose!\nWould You Like to Try Again?")){
+                    guess = 0
+                    hangman()
+                } else {
+                    $('#buttons').hide()
+                    $('#letters').hide()
+                    // display a lose page?
+                }
+            } 
         }
     })
 })
